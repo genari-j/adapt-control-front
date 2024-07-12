5ed290732f787ae0e175bb9b5af8d69db37d4fe2
João Genari <victor.olr@hotmail.com>
2024-07-12 14:23:27 -0300

    feat: user department name

--- src/pages/users/profile/index.tsx
+++ src/pages/users/profile/index.tsx
@@ -19,8 +19,8 @@ export const Profile = () => {
           <span>Código: {userInfos?.sub}</span>
           <span>Nome: {userInfos?.name}</span>
           <span>E-mail: {userInfos?.email}</span>
-          <span>Departamento: {userInfos?.departments.id !== 0 ? 'Tem departamento' : 'Não tem departamento'}</span>
-          <span>Situação: {userInfos?.active !== 0 ? 'Ativo' : 'Não ativo'}</span>
+          <span>Departamento: {userInfos?.departments.name}</span>
+          <span>Situação: {userInfos?.active !== 0 ? 'Funcionário ativo' : 'Funcionário não ativo'}</span>
           <span>Foto: {userInfos?.avatar !== null ? userInfos?.avatar : 'Sem foto'}</span>
           <span>Criação: {new Date(String(userInfos?.created_at)).toLocaleDateString('pt-BR')}</span>
         </UserInfos>
