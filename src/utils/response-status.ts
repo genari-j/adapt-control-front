1148b8056e00dcf8a1a7bef71b85a80e41330f8f
João Genari <victor.olr@hotmail.com>
2025-03-14 21:48:00 -0300

    refactor: adjusting global util to api responses

--- src/utils/response-status.ts
+++ src/utils/response-status.ts
@@ -1,37 +1,29 @@
-import { AxiosError } from 'axios'
+import type { AxiosError } from 'axios'
+
 import { toast } from 'react-hot-toast'
 
-export const responseStatus = (error: AxiosError) => {
-  const message: Record<number, string> = {
-    400: 'Ocorreu algum erro com a solicitação.',
-    401: 'Não autorizado.',
-    404: 'Solicitação não encontrada.',
-    500: 'Ops, houve um erro ao tentar conexão com o servidor.'
-  }
+import type { ApiResponse } from '~/@types'
 
-  if (!error.response) {
-    toast.error('Algo inesperado ocorreu.')
-  } else {
-    toast.error(message[error.response.status])
-  }
+type ResponseType = {
+	[key: number]: string
 }
 
-// export const responseStatus = (error: AxiosError) => {
-//   switch (error?.response?.status) {
-//     case 500:
-//       toast.error('Ops, houve um erro ao tentar conexão com o servidor.')
-//       break
-//     case 404:
-//       toast.error('Solicitação não encontrada.')
-//       break
-//     case 400:
-//       toast.error('Ocorreu algum erro com a solicitação.')
-//       break
-//     case 401:
-//       toast.error('Não autorizado.')
-//       break
-//     default:
-//       console.log(`Erro: ${error?.response?.status}`)
-//       break;
-//   }
-// }
+export const responseStatus = (error: AxiosError) => {
+	const errorMessages: ResponseType = {
+		400: 'Solicitação inválida.',
+		401: 'Não autorizado.',
+		404: 'Solicitação não encontrada.',
+		500: 'Ops, houve um erro ao tentar conectar ao servidor.',
+	}
+
+	if (!error.response) {
+		toast.error('Ocorreu um erro. Não foi possível estabelecer uma conexão com o servidor.')
+	} else {
+		const statusCode = error.response.status
+		const apiMessage = (error.response.data as ApiResponse).message
+
+		const message = apiMessage || errorMessages[statusCode] || 'Ocorreu um erro inesperado.'
+
+		toast.error(message)
+	}
+}
